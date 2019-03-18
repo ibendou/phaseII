5ef67ca18c2fbaad31792a0c0c7de20bf27fae66
Imane Bendou <ibendou@andrew.cmu.edu>
2019-03-18 17:31:32 +0000

    added contexts

--- test/contexts.rb
+++ test/contexts.rb
@@ -1,49 +1,37 @@
 module Contexts
   def create_employees
     @alex = FactoryBot.create(:employee)
-    @imane = FactoryBot.create(:employee, first_name: "Imane", last_name: "Bendou", ssn: "023455320", date_of_birth: 1976-01-09, phone:"433488211", role: "manager"  active: false)
+    @imane = FactoryBot.create(:employee, first_name: "Imane", last_name: "Bendou", ssn: "02345320", date_of_birth: 1976-01-09, phone:"4334978211", role: "manager",  active: false)
     @mark = FactoryBot.create(:employee, first_name: "Mark",ssn: "021234520", phone: "433448711")
   end
   
-  def destroy_children
+  def destroy_employees
     @alex.destroy
-    @rachel.destroy
+    @imane.destroy
     @mark.destroy
   end
   
-  def create_tasks
-    @dishes = FactoryBot.create(:task)
-    @wood = FactoryBot.create(:task, name: "Stack wood", active: false)
-    @sweep = FactoryBot.create(:task, name: "Sweep floor")
-    @shovel = FactoryBot.create(:task, name: "Shovel driveway", points: 3)
-    @mow = FactoryBot.create(:task, name: "Mow grass", points: 2)
+  def create_stores
+    @cmu = FactoryBot.create(:store)
+    @texas = FactoryBot.create(:store, name: "Texas", street: "Some Street", state: "OH", city: "Alexandria", zip: "43001", longitude: "45.98755", latitude:"43.987643", active: false)
+    @damam = FactoryBot.create(:store, name: "Damam", street: "street2", state: "WV", city: "Accoville",zip: "25606",longitude: "80.98755", latitude:"70.987643" )
+
   end
   
-  def destroy_tasks
-    @dishes.destroy
-    @mow.destroy
-    @wood.destroy
-    @sweep.destroy
-    @shovel.destroy
+  def destroy_stores
+    @cmu.destroy
+    @texas.destroy
+    @damam.destroy
   end
   
-  def create_chores
-    @ac1 = FactoryBot.create(:chore, child: @alex, task: @dishes)
-    @mc1 = FactoryBot.create(:chore, child: @mark, task: @sweep)
-    @ac2 = FactoryBot.create(:chore, child: @alex, task: @sweep, due_on: 2.days.from_now.to_date)
-    @mc2 = FactoryBot.create(:chore, child: @mark, task: @dishes, due_on: 2.days.from_now.to_date)
-    @ac3 = FactoryBot.create(:chore, child: @alex, task: @shovel, due_on: 2.days.ago.to_date, completed: true)
-    @ac4 = FactoryBot.create(:chore, child: @alex, task: @dishes, due_on: Date.today, completed: true)
-    @mc3 = FactoryBot.create(:chore, child: @mark, task: @sweep, due_on: Date.today, completed: true)
+  def create_assignments
+    @assignment1 = FactoryBot.create(:assignment, employee: @alex, store: @cmu)
+    @assignment1 = FactoryBot.create(:assignment, employee: @mark, store: @texas)
+    @assignment1  = FactoryBot.create(:assignment, employee: @imane, store: @damam, end_date: 2012-01-31, pay_level: 6)
+
   end
   
-  def destroy_chores
-    @ac1.destroy
-    @ac2.destroy
-    @ac3.destroy
-    @ac4.destroy
-    @mc1.destroy
-    @mc2.destroy
-    @mc3.destroy
+  def destroy_assignments
+
   end
 end
--- test/models/assignment_test.rb
+++ test/models/assignment_test.rb
@@ -4,4 +4,15 @@ class AssignmentTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  
+   scope :current, -> { where('end_date == nil') }
+   scope :past, -> { where('end_date < ?', Date.today) }
+   scope :for_store, (store_id) -> {}
+   scope :for_employee, (employee_id) -> {}
+   scope :for_pay_level, (pay_level) -> {}
+   scope :for_role, (role) -> {}
+   scope :by_store, (store_id) -> {}
+   scope :by_employee, (last, first) -> {}
+   scope :chronological, -> {order('end_date')}
+
 end
